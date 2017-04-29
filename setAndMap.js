131707538fe10bdd2170ca23e18b381ab3b7bb56
SANTOSH HUMNEKAR <santosh.humnekar@gmail.com>
2017-04-29 17:16:44 +0530

    Update setAndMap.js

--- setAndMap.js
+++ setAndMap.js
@@ -1,18 +1,11 @@
 // ==================================Map and Set=============================================
-
-// Create map instance object using new Map() and set value using set(). 
-//Add values here(Key can be string, object and function.)
-
 let objMapBankAccount = new Map();
-objMapBankAccount.set(1, 'AccountSaving');
-objMapBankAccount.set(2, 'AccountCurrent');
-objMapBankAccount.set(3, 'AccountSalary');
+objMapBankAccount.set(1, 'SavingAccount'); // map instance object using new Map() and set value using set()
+objMapBankAccount.set(2, 'CurrentAccount'); //Add values here(Key can be string, object and function.)
+objMapBankAccount.set(3, 'SalaryAccount');
 
 
-//Create Set() instance and then add values using add() method    
-//Note: Check new property before adding, if it’s there or not using has() method.
-
-let objSetBankAccount = new Set();
+let objSetBankAccount = new Set(); //Add values here(Key can be string, object and function.)
 
 if (!objSetBankAccount.has("AcSaving")) {
     objSetBankAccount.add("AcSaving");
@@ -27,11 +20,9 @@ if (!objSetBankAccount.has("AcSalary")) {
 }
 
 
-// /Add multiple values using Set(), then iterate them using for loop. 
+let Accounts = ["Saving", "Current", "Salary"]; //Add multiple values using Set(), then iterate them using for loop. 
 
-let banks = ["SBI", "ICICI", "HDFC"];
-
-banks.forEach((str) => {
+Accounts.forEach((str) => {
     if (!objSetBankAccount.has(str)) {
         objSetBankAccount.add(str);
     }
@@ -42,26 +33,25 @@ objSetBankAccount.forEach((ele) => {
 });
 
 
-// Create an array(stringArray) of strings  then set this array to Set instance using:   var mySet = new Set(stringArray); 
-//  Now check if all values are there or not using set’s has() method. Print all values of array in one time(using spread operator) 
 
-let allBank = [];
-objMapBankAccount.forEach((value, key, map) => {
-    allBank.push(value);
+let allAccounts = [];
+objMapBankAccount.forEach((value, key, map) => { // an array(stringArray) of strings  then set this array to Set instance using:   var mySet = new Set(stringArray); 
+    allAccounts.push(value); // Now check if all values are there or not using set’s has() method. Print all values of array in one time(using spread operator) 
+
 });
 
 objSetBankAccount.forEach((ele) => {
-    allBank.push(ele);
+    allAccounts.push(ele);
 });
 
-let allBankSet = new Set(allBank);
+let allAccountsSet = new Set(allAccounts);
 
-allBank.forEach((ele) => {
+allAccounts.forEach((ele) => {
 
-    if (!allBankSet.has(ele)) {
-        console.log(`one Bank is missing in SET ${ele}`);
+    if (!allAccountsSet.has(ele)) {
+        console.log(`This Bank is missing in Set ${ele}`);
     } else {
-        console.log(` ${ele}  Bank is present in SET`);
+        console.log(` ${ele}  This Bank is Presen in Set`);
     }
 });
 
@@ -71,4 +61,4 @@ let printarr = (...args) => {
     console.log(args);
 }
 
-printarr(...allBankSet);
+printarr(...allAccountsSet);
